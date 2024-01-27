263503e7cfe0a234b76bc34a4b007c71159601a5
Leslie Lopez <leslie2515lopez@gmail.com>
2024-01-26 23:01:18 -0600

    Finished setting up the pieces.js file

---
+++ app.js
@@ -0,0 +1,6 @@
+const gameBoard = document.querySelector("#gameboard");
+const playerDisplay = document.querySelector("#player");
+const infoDisplay = document.querySelector("#info-display");
+const width = 8;
+
+const startPieces = [];
